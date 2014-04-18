cd4df2415b8005973719027423b0c153b036ac84
Chris Rice <crice@mines.edu>
2014-04-18 14:34:38 -0600

    Move validity completed

--- nim.hs
+++ nim.hs
@@ -7,27 +7,33 @@ displayBoard :: Board -> IO ()
 displayBoard board = do
 	mapM_ (putStrLn) $ map (\x -> replicate x '|') board
 
-validRow :: Int -> Board -> Bool
-validRow row board = (row < (length board)) && (not emptyRow)
-	where emptyRow = board !! row == 0
+validMove :: Int -> Int -> Board -> Bool
+validMove row number board = validRow && hasSticks && (not emptyRow)
+	where emptyRow = (board !! row == 0)
+	      validRow = (row < (length board))
+	      hasSticks = (board !! row) >= number
+
 
 removeSticks :: Int -> Int -> Board -> Board
 removeSticks row number board = board
 
-
 game :: Board -> IO()
 game board = do
 	displayBoard board
+	
 	putStr "Enter Row: "
 	row <- getLine
-	let valid =  validRow (read row) board
+	putStr "Enter Sticks to take: "
+	number <- getLine
+
+	let valid =  validMove (read row) (read number) board
 	if (valid)
-		then do putStrLn "Valid Row";
+		then do putStrLn "Valid Move!";
 			-- Prompt for and remove n sticks
 			-- Make computer move
 			-- call game with the modified board
 		else do 
-			putStrLn "Please enter a valid row.";
+			putStrLn "Please enter a valid move.";
 			game board
 	
 
